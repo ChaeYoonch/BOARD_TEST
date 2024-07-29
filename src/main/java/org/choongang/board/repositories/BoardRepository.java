9a888788cd4d22678fe4e2c1ca01fd1bf1b1db9a
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-29 23:19:14 +0900

    BoardRepository

--- src/main/java/org/choongang/board/repositories/BoardRepository.java
+++ src/main/java/org/choongang/board/repositories/BoardRepository.java
@@ -1,5 +1,9 @@
 package org.choongang.board.repositories;
 
-public class BoardRepository {
+import org.choongang.board.entities.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
+public interface BoardRepository extends JpaRepository<Board, Long>, QuerydslPredicateExecutor<Board>  {
+    Board findByTitle(String title);
 }
