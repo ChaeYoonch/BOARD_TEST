6e5e1582a7a5f76d6bbbb4fe0cc4f79ac60b8f93
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-29 23:05:08 +0900

    Board

--- src/main/java/org/choongang/board/entities/Board.java
+++ src/main/java/org/choongang/board/entities/Board.java
@@ -1,5 +1,16 @@
 package org.choongang.board.entities;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Board {
 
 }
