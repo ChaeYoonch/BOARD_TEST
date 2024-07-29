48549f7da294ac0ea6900ec0119b488d80590b0f
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-29 23:05:54 +0900

    Board

--- src/main/java/org/choongang/board/entities/Board.java
+++ src/main/java/org/choongang/board/entities/Board.java
@@ -1,6 +1,9 @@
 package org.choongang.board.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,4 +16,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Board {
 
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
 }
