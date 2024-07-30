688cd52b466ede43a18ab83a9e26505ece8a1506
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-30 11:41:08 +0900

    RequestBoard

---
+++ src/main/java/org/choongang/board/controllers/RequestBoard.java
@@ -0,0 +1,18 @@
+package org.choongang.board.controllers;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class RequestBoard {
+
+    @NotBlank @Size(min=2) // 아이디 : 최소 2자리 이상
+    private String id; // 아이디
+
+    @NotBlank @Size(min=1, max=10) // 제목 : 최소 1자리 최대 10자리
+    private String title; // 제목
+
+    @NotBlank @Size(min = 1, max = 30) // 내용 : 최소 1자리 최대 30자리
+    private String content; // 내용
+}
