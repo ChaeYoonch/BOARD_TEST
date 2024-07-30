2433012f85fcca75cf803e46fa2999250d1a1684
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-30 11:36:29 +0900

    Board

--- src/main/java/org/choongang/board/entities/Board.java
+++ src/main/java/org/choongang/board/entities/Board.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
