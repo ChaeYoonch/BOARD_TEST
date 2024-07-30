ecb9ad3877f2c3c2ca13f6c359a6a97a88aba3aa
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-30 10:25:28 +0900

    BoardListService

--- src/main/java/org/choongang/board/services/BoardListService.java
+++ src/main/java/org/choongang/board/services/BoardListService.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 @Transactional
-public class BoardInfoService {
+public class BoardListService {
     private final BoardRepository boardRepository;
 
     public List<Board> getBoard() {
