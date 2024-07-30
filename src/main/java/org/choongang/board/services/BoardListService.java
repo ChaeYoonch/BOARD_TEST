6f56afd630be97d6d0b8ea1dd23912500d42b445
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-30 17:25:01 +0900

    BoardListService

--- src/main/java/org/choongang/board/services/BoardListService.java
+++ src/main/java/org/choongang/board/services/BoardListService.java
@@ -2,6 +2,7 @@ package org.choongang.board.services;
 
 import lombok.RequiredArgsConstructor;
 import org.choongang.board.entities.Board;
+import org.choongang.board.exceptions.BoardNotFoundException;
 import org.choongang.board.repositories.BoardRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,11 +17,13 @@ public class BoardListService {
 
     // 전체 게시글 목록 가져오기
     public List<Board> getBoard() {
-        return boardRepository.findAll();
+        return boardRepository.findAllByOrderByIdAsc();
     }
 
     // 게시글 1개 가져오기
     public Board getBoards(Long id) {
+        Board board = boardRepository.findById(id).orElseThrow(BoardNotFoundException::new);
+
         return boardRepository.findById(id).orElse(null);
     }
 }
