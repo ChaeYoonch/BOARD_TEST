1df527bea1a5409d7648c7135e45863f57ea196d
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-30 09:13:31 +0900

    SecurityConfig : 인증 무력화

--- src/main/java/org/choongang/board/configs/SecurityConfig.java
+++ src/main/java/org/choongang/board/configs/SecurityConfig.java
@@ -1,9 +1,11 @@
 package org.choongang.board.configs;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 public class SecurityConfig {
 
     @Bean // 스프링 관리 객체로 설정
