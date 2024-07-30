78ef1a53ce0e6f4c772b0849f17f1068eff22819
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-30 10:23:50 +0900

    BoardInfoService

---
+++ src/main/java/org/choongang/board/services/BoardInfoService.java
@@ -0,0 +1,24 @@
+package org.choongang.board.services;
+
+import lombok.RequiredArgsConstructor;
+import org.choongang.board.entities.Board;
+import org.choongang.board.repositories.BoardRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class BoardInfoService {
+    private final BoardRepository boardRepository;
+
+    public List<Board> getBoard() {
+        return boardRepository.findAll();
+    }
+
+    public Board getBoards(Long id) {
+        return boardRepository.findById(id).orElse(null);
+    }
+}
